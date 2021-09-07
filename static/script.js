3afc4d414109db16cced83210ea9a71e0f26301d
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-09-07 06:53:26 -0400

    huiehwehweoeihwefoioi.

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
-function unfinished() {
-    window.location.replace("../unfinished.html");
+function unfinished(path) {
+    var location = "${path}"
+    console.log(location);
+    window.location.replace("https://NathanMcMillan54.github.io/unfinished.html");
 }
